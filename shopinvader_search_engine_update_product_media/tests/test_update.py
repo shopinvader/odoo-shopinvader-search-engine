d567bd8f43f01a585669cb432ed213b8404c86ad
Florian Mounier <paradoxxx.zero@gmail.com>
2026-03-27 11:04:36 +0100

    [MIG] shopinvader_search_engine_update_product_media: Migration to 18.0

--- shopinvader_search_engine_update_product_media/tests/test_update.py
+++ shopinvader_search_engine_update_product_media/tests/test_update.py
@@ -2,35 +2,33 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import base64
 
-from odoo.addons.shopinvader_search_engine.tests.common import TestProductBindingMixin
 from odoo.addons.shopinvader_search_engine_product_media.tests.common import (
     ProductMediaCase,
 )
 
 
-class TestUpdate(ProductMediaCase, TestProductBindingMixin):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        TestProductBindingMixin.setup_records(cls)
-        cls.env["fs.product.media"].create(
+class TestUpdate(ProductMediaCase):
+    def setup_records(self, backend=None):
+        rv = super().setup_records(backend=backend)
+        self.env["fs.product.media"].create(
             {
-                "product_tmpl_id": cls.product.product_tmpl_id.id,
-                "media_id": cls.media_c.id,
+                "product_tmpl_id": self.product_a.product_tmpl_id.id,
+                "media_id": self.media_c.id,
                 "sequence": 10,
                 "link_existing": True,
             }
         )
-        cls.product_binding.state = "done"
+        self.product_binding.state = "done"
+        return rv
 
     def test_unlink_media(self):
-        self.product.media_ids.unlink()
+        self.product_a.media_ids.unlink()
         self.assertEqual(self.product_binding.state, "to_recompute")
 
     def test_add_media(self):
         self.env["fs.product.media"].create(
             {
-                "product_tmpl_id": self.product.product_tmpl_id.id,
+                "product_tmpl_id": self.product_a.product_tmpl_id.id,
                 "file": {
                     "filename": "new.txt",
                     "content": base64.b64encode(b"new media"),
@@ -41,5 +39,5 @@ class TestUpdate(ProductMediaCase, TestProductBindingMixin):
         self.assertEqual(self.product_binding.state, "to_recompute")
 
     def test_update_media(self):
-        self.product.media_ids[0].media_type_id = self.media_type_a.id
+        self.product_a.media_ids[0].media_type_id = self.media_type_a.id
         self.assertEqual(self.product_binding.state, "to_recompute")
