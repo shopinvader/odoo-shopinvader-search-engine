454050c5312225f445356d5b196d2751172da7bb
Florian Mounier <paradoxxx.zero@gmail.com>
2026-03-27 10:57:37 +0100

    [MIG] shopinvader_search_engine_product_template_multi_link: Migration to 18.0

--- shopinvader_search_engine_product_template_multi_link/tests/test_product.py
+++ shopinvader_search_engine_product_template_multi_link/tests/test_product.py
@@ -2,10 +2,10 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from .common import TestProductLinkCase
+from .common import TestProductLinkBase
 
 
-class TestProductLink(TestProductLinkCase):
+class TestProductLinkCase(TestProductLinkBase):
     def _assert_links(self, binding, expected):
         binding.with_context(index=self.se_product_index).recompute_json()
         self.assertEqual(binding.data["links"], expected)
