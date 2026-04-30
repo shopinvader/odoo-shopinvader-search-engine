51daa837e1e6bfbc78b8d1282c2e668f33ecd3f5
Florian Mounier <paradoxxx.zero@gmail.com>
2026-04-30 10:37:04 +0200

    [MIG] shopinvader_search_engine: Migration to 18.0

--- shopinvader_search_engine/__manifest__.py
+++ shopinvader_search_engine/__manifest__.py
@@ -2,10 +2,9 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 {
     "name": "Shopinvader Catalog Search Engine Connector",
-    "version": "16.0.1.0.7",
+    "version": "18.0.1.0.0",
     "author": "Akretion",
     "website": "https://github.com/shopinvader/odoo-shopinvader-search-engine",
     "license": "AGPL-3",
@@ -15,7 +14,6 @@
         "shopinvader_product",
     ],
     "data": [],
-    "installable": False,
-    "application": False,
+    "installable": True,
     "development_status": "Alpha",
 }
